de1a68916b4dbeff3794109117d5667b4ab99eef
sarragobji <sarra.gobji2@gmail.com>
2026-09-09 19:14:36 +0100

    enabled cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://127.0.0.1:5173',
+    ],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
